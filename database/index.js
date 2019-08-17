a9c34b9b5b646d04860c09639cea11b2393ce0d4
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-16 21:44:55 -0700

    Added database schema and 'save' logic; troubleshooting connection

--- database/index.js
+++ database/index.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.createConnection('mongodb://localhost/fetcher', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("CONNNNNNNNECTED");
+});
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner: String,
+  description: String,
+  html_url: String,
+  commits_url: String,
+  commits_quantity: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repo, callback) => {
+    let test = new Repo({
+      id: repo.id,
+      name: repo.name,
+      owner: repo.owner.name,
+      description: repo.description,
+      html_url: repo.html_url,
+      commits_url: repo.commits_url,
+      commits_quantity: repo.commits_quantity
+    })
+    console.log("HITTING SAVE");
+    test.save();
+
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
