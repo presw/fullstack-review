e1ef30270d35c75cbb555f46c95704ed2f783b52
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-16 21:46:01 -0700

    Added getReposByUsername and getCommitsByRepo

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,48 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
+let getReposByUsername = (username, callback) => {
 
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${username}/repos`, //gets REPOS for a user
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
 
+  request(options, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, body);
+    }
+  })
+  // TODO - Use the request module to request repos for a specific
+  // user from the github API
+
+  // The options object has been provided to help you out,
+  // but you'll have to fill in the URL
+}
+
+let getCommitsByRepo = (commitsUrl, callback) => {
+
+  let options = {
+    url: `${commitsUrl}`,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${config.TOKEN}`
+    }
+  };
+
+  request(options, (error, response, body) => {
+    if (error) {
+      callback(error, null)
+    } else {
+      callback(null, body);
+    }
+  })
 }
 
 module.exports.getReposByUsername = getReposByUsername;
+module.exports.getCommitsByRepo = getCommitsByRepo;
